d404924889e1f097241c7e90bb61b96635778493
RollyLif <relifungula@gmail.com>
2022-10-19 16:11:40 +0200

    fix : removing unused variables

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 4000;
-const sikoyo = new Date();
-const heure   = sikoyo.getHours();
-const minute  = sikoyo.getMinutes();
-const seconde = sikoyo.getSeconds();
+
 
 app.get('/', (req, res) => {
 res.sendFile(__dirname + "/views/index.html");
